a2b097b86bade4098b7d337dc55d80150a824e5e
Himas1321 <nikita5045@mail.ru>
2025-09-11 23:07:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ with open("file/coffee.json", "r", encoding="CP1251") as coffee:
 
 file_coffee = json.loads(coffee_json)
 
-# print(file_coffee)
-# print(file_coffee["Name"])
-first = file_coffee[0]['geoData']
-first_b = first['coordinates'] 
-# first_a = first['Name']
-# file = first['PublicPhone']
-# file_a = file[0]
-# file_b = file_a['geoData']
-# file_g = file_b[0]
-# first_d = file_g['coordinates']
-print(first_b)
-# print(file_d)
+
+coffee_shop = file_coffee[1]['Name']
+print(coffee_shop)
+for cafe in file_coffee:
+	coffee_shop = file_coffee[0]['Name']
+	print(coffee_shop)
+
+coordinates = file_coffee[0]['geoData']['coordinates']
+
+
+
+# print(coffee_shop, '\n', coordinates[0], '\n', coordinates[1])
+# print(coordinates[0])
+# print(coordinates[1])
+# print(coffee_shop,'\n', coordinates)
