e8fd00ca08783ab194c14424ebf8ccf6f8dd97c9
Himas1321 <nikita5045@mail.ru>
2025-09-11 15:58:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ file_coffee = json.loads(coffee_json)
 
 # print(file_coffee)
 # print(file_coffee["Name"])
-first = file_coffee[0]
-first_a = first['Name']
-print(first_a) 
+first = file_coffee[0]['geoData']
+first_b = first['coordinates'] 
+# first_a = first['Name']
+# file = first['PublicPhone']
+# file_a = file[0]
+# file_b = file_a['geoData']
+# file_g = file_b[0]
+# first_d = file_g['coordinates']
+print(first_b)
+# print(file_d)
