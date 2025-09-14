2e0c1575bef991abfdef3887f94b6345e7e6ba33
Himas1321 <nikita5045@mail.ru>
2025-09-14 21:56:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
+import os
 import json
+import requests
+
+apikey = '523415ff-cec5-4774-b24a-fd5d9203c5d0'
 
 
-with open("file/coffee.json", "r", encoding="CP1251") as coffee:
-	coffee_json = coffee.read()
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
-file_coffee = json.loads(coffee_json)
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+ 
+# def user(apikey):
+# 	name_user = input('Где вы находитесь: ')
+# 	coords = fetch_coordinates(apikey, name_user)
+# 	print(f'Ваши координаты: {coords}')   
+
+def main():
+	name_user = input('Где вы находитесь: ')
+	coords = fetch_coordinates(apikey, name_user)
+	print(f'Ваши координаты: {coords}') 
+
+	with open("file/coffee.json", "r", encoding="CP1251") as coffee:
+		coffee_json = coffee.read()
+		
+	file_coffee = json.loads(coffee_json)
+
+	for cafe in file_coffee:
+		if 'Name' in cafe:
+			print(cafe['Name'])
+			if 'geoData' in cafe and 'coordinates' in cafe['geoData']:
+				print(cafe['geoData']['coordinates'])
 
 
 
-# print(coffee_shop)
-for cafe in file_coffee:
-	if cafe == 'Name':
-		print(cafe)
 
+# # coffee_shop = file_coffee[1]['Name']
+# # coordinates = file_coffee[0]['geoData']['coordinates']
 
-coffee_shop = file_coffee[1]['Name']
-coordinates = file_coffee[0]['geoData']['coordinates']
-
+if __name__ == '__main__':
+	main()
