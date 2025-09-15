e31937a4a6182d67fdb2146d5a13dba3db339903
Himas1321 <nikita5045@mail.ru>
2025-09-15 21:25:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+from geopy import distance
 
 apikey = '523415ff-cec5-4774-b24a-fd5d9203c5d0'
 
@@ -22,33 +23,47 @@ def fetch_coordinates(apikey, address):
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
     return lon, lat
 
+
+
+
  
 # def user(apikey):
 # 	name_user = input('Где вы находитесь: ')
 # 	coords = fetch_coordinates(apikey, name_user)
 # 	print(f'Ваши координаты: {coords}')   
 
+
 def main():
-	name_user = input('Где вы находитесь: ')
-	coords = fetch_coordinates(apikey, name_user)
-	print(f'Ваши координаты: {coords}') 
+	name_a = input('Пункт А: ')
+	name_b = input('Пункт Б: ')
+	coords_a = fetch_coordinates(apikey, name_a)
+	coords_b = fetch_coordinates(apikey, name_b)
+	coords_a_1 = coords_a[1], coords_a[0]
+	coords_b_1 = coords_b[1], coords_b[0]
+	result_distance = distance.distance(coords_a_1, coords_b_1).km
+	print(f'Расстояние: {result_distance}')
+	print(f'Ваши координаты: {coords_b[1], coords_b[0]}, {coords_a[1], coords_a[0]}') 
 
 	with open("file/coffee.json", "r", encoding="CP1251") as coffee:
 		coffee_json = coffee.read()
 		
 	file_coffee = json.loads(coffee_json)
 
-	for cafe in file_coffee:
-		if 'Name' in cafe:
-			print(cafe['Name'])
-			if 'geoData' in cafe and 'coordinates' in cafe['geoData']:
-				print(cafe['geoData']['coordinates'])
+	coffeeshop_list = []
+
+# 	for cafe in file_coffee:
+# 		if 'Name' in cafe:
+# 			print(cafe['Name'])
+# 			if 'geoData' in cafe and 'coordinates' in cafe['geoData']:
+# 				print(cafe['geoData']['coordinates'])
 
 
 
 
-# # coffee_shop = file_coffee[1]['Name']
-# # coordinates = file_coffee[0]['geoData']['coordinates']
+# # # coffee_shop = file_coffee[1]['Name']
+# # # coordinates = file_coffee[0]['geoData']['coordinates']
 
 if __name__ == '__main__':
 	main()
+	main()
+
