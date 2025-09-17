324db1c70dc1a2a03060b3dfafcc822bd4ad5611
Himas1321 <nikita5045@mail.ru>
2025-09-17 22:10:33 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
+from dotenv import load_dotenv
 import json
 import requests
 from geopy import distance
 from pprint import pprint
-
-apikey = '523415ff-cec5-4774-b24a-fd5d9203c5d0'
+import folium
 
 
 def fetch_coordinates(apikey, address):
@@ -25,32 +25,17 @@ def fetch_coordinates(apikey, address):
     return lon, lat
 
 
-
-
- 
-# def user(apikey):
-# 	name_user = input('Где вы находитесь: ')
-# 	coords = fetch_coordinates(apikey, name_user)
-# 	print(f'Ваши координаты: {coords}')   
 def get_coffeeshop_distance(coffeeshop_min):
 	return coffeeshop_min['distance']
 
+
 def main():
-	# name_a = input('Пункт А: ')
-	# name_b = input('Пункт Б: ')
-	# coords_a = fetch_coordinates(apikey, name_a)
-	# coords_b = fetch_coordinates(apikey, name_b)
-	# coords_a_1 = coords_a[1], coords_a[0]
-	# coords_b_1 = coords_b[1], coords_b[0]
-	# result_distance = distance.distance(coords_a_1, coords_b_1).km
-	# print(f'Расстояние: {result_distance}')
-	# print(f'Ваши координаты: {coords_b[1], coords_b[0]}, {coords_a[1], coords_a[0]}')
+	load_dotenv()
+	apikey = os.getenv('APIKEY')
 
 	user = input('Где вы находитесь ?')
 	coords = fetch_coordinates(apikey, user)
 	coords_separately = coords[1], coords[0]
-	# result_distance = distance.distance(coords_separately).km
-	print(f'Ваши координаты: {coords[1], coords[0]}')
 
 	with open("file/coffee.json", "r", encoding="CP1251") as coffee:
 		coffee_json = coffee.read()
@@ -63,31 +48,33 @@ def main():
 		coffeeshop_coords = coffeeshop['geoData']['coordinates']
 		coffeeshop_info = dict()
 		coffeeshop_info['title'] = coffeeshop['Name']
-		coffeeshop_info['distance'] = distance.distance(
-			(coffeeshop_coords[1], coffeeshop_coords[0]),
-			(coords_separately)).km
+		coffeeshop_info['distance'] = distance.distance((coffeeshop_coords[1], coffeeshop_coords[0]), (coords_separately)).km
 		coffeeshop_info['latitude'] = coffeeshop_coords[1]
 		coffeeshop_info['longitude'] = coffeeshop_coords[0]
 		coffeeshop_list.append(coffeeshop_info)
 
 	coffeeshop_min = sorted(coffeeshop_list, key=get_coffeeshop_distance)[:5]
 	
+	m = folium.Map([coords[1], coords[0]], zoom_start=12)
+
+	folium.Marker(
+		location=[coords[1], coords[0]],
+		tooltip="Нажми меня!",
+		popup="Я",
+		icon=folium.Icon(icon="green"),
+	).add_to(m)
+
+	for coffeeshop in coffeeshop_min:
+		folium.Marker(
+            location=[coffeeshop['latitude'], coffeeshop['longitude']],
+            tooltip=coffeeshop['title'],
+            popup=coffeeshop['title'],
+            icon=folium.Icon(color='red'),
+        ).add_to(m)
+
+	m.save('index.html')
 	pprint(coffeeshop_min)
 
-	# for cafe in file_coffee:
-	# 	if 'Name' in cafe:
-	# 		print(cafe['Name'])
-	# 		if 'geoData' in cafe and 'coordinates' in cafe['geoData']:
-	# 			print(cafe['geoData']['coordinates'])
-
-
-
-
-
-
-# # # coffee_shop = file_coffee[1]['Name']
-# # # coordinates = file_coffee[0]['geoData']['coordinates']
 
 if __name__ == '__main__':
 	main()
-
